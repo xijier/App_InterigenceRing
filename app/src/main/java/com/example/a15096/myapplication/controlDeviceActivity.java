ffa9fbad874cd999731289488fb787f7d236f54e
xijier <aola.xijier@gmail.com>
2017-12-04 22:38:42 +0800

    SharedPreferences

--- app/src/main/java/com/example/a15096/myapplication/controlDeviceActivity.java
+++ app/src/main/java/com/example/a15096/myapplication/controlDeviceActivity.java
@@ -2,23 +2,20 @@ package com.example.a15096.myapplication;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
 
+import java.util.Iterator;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
-
-public class deviceSetActivity extends AppCompatActivity {
+public class controlDeviceActivity extends AppCompatActivity {
     private ListView list_one;
     private MyAdapter mAdapter = null;
     private List<Data> mData = null;
@@ -26,35 +23,20 @@ public class deviceSetActivity extends AppCompatActivity {
     private Button btn_add;
     private Button btn_remove;
     private int flag = 1;
-    private Data mData_5 = null;   //用来临时放对象的
+    private Data mData_5 = null;
     private final static String PREFRENCE_FILE_KEY = "com.example.a15096.shared_preferences";
     private SharedPreferences mSharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_device_set);
-        mContext = deviceSetActivity.this;
+        setContentView(R.layout.activity_control_device);
+        mContext = controlDeviceActivity.this;
         mSharedPreferences=getSharedPreferences(PREFRENCE_FILE_KEY, Context.MODE_PRIVATE);
         bindViews();
         mData = new LinkedList<Data>();
         mAdapter = new MyAdapter((LinkedList<Data>) mData,mContext);
         list_one.setAdapter(mAdapter);
-        btn_add = (Button) findViewById(R.id.btn_add);
-        btn_remove = (Button) findViewById(R.id.btn_remove);
-        btn_add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                adddeviceSet();
-            }
-        });
-        btn_remove.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                removedeviceSet(view);
-            }
-        });
-
         Button button_DeviceSetButton = (Button) findViewById(R.id.buttonDeviceSet);
         button_DeviceSetButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,9 +59,9 @@ public class deviceSetActivity extends AppCompatActivity {
             Iterator iter = map.entrySet().iterator();
             while (iter.hasNext()) {
                 Map.Entry entry = (Map.Entry) iter.next();
-                String key = (String) entry.getKey();
-                String val = (String) entry.getValue();
-                mData_5 = new Data(R.mipmap.ic_launcher,key + flag);
+               String key = (String) entry.getKey();
+                //String val = (String) entry.getValue();
+                mData_5 = new Data(R.mipmap.ic_launcher, key + flag);
                 mAdapter.add(mData_5);
                 flag++;
             }
@@ -104,24 +86,6 @@ public class deviceSetActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    private void adddeviceSet()
-    {
-        mData_5 = new Data(R.mipmap.ic_launcher,"给希哥跪了~~~ x " + flag);
-        mAdapter.add(mData_5);
-        flag++;
-       final SharedPreferences.Editor editor = mSharedPreferences.edit();
-        editor.putString(mData_5.getContent(),"");
-        //editor.putInt("age",24);
-        editor.apply();
-
-    }
-    private void removedeviceSet(View v)
-    {
-        if(v.getId()==R.id.btn_remove)
-        {
-            mAdapter.remove(mData_5);
-        }
-    }
     private void bindViews(){
         list_one = (ListView) findViewById(R.id.list_one);
     }
